6f5fa604aa823989753a302b462f7b94fe6df678
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-18 02:20:06 +0600

    Configured sound playing functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,13 +9,68 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: Text(
-              nouns.first,
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FlatButton(
+                onPressed: () {
+                  _playSound(1);
+                },
+                child: null,
+                color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: () {
+                  _playSound(2);
+                },
+                child: null,
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {
+                  _playSound(3);
+                },
+                child: null,
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: () {
+                  _playSound(4);
+                },
+                child: null,
+                color: Colors.lightGreen,
+              ),
+              FlatButton(
+                onPressed: () {
+                  _playSound(5);
+                },
+                child: null,
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: () {
+                  _playSound(6);
+                },
+                child: null,
+                color: Colors.blue,
+              ),
+              FlatButton(
+                onPressed: () {
+                  _playSound(7);
+                },
+                child: null,
+                color: Colors.purple,
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void _playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
   }
 }
